c64648da945765d5fa0e0e31cd5d8bb60344e73f
Nitzan <bresler.nitzan@gmail.com>
2025-10-06 12:33:59 +0300

    Panic!

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ use std::result::Result;
 use std::{env, io, path::PathBuf};
 use tokio::time::Duration;
 
+use std::net::TcpListener;
+
+fn is_port_in_use(port: u16) -> bool {
+    TcpListener::bind(("127.0.0.1", port)).is_err()
+}
 fn find_device_path_by_name(device_query: &str) -> io::Result<PathBuf> {
     let device_query_lower = device_query.to_lowercase();
 
@@ -70,19 +75,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
     println!("Using device: {:?}", path);
 
-    Command::new("openrgb")
-        .arg("--server")
-        .stdin(Stdio::null())
-        .spawn()?;
+    if is_port_in_use(6742) {
+        println!("OpenRGB server is already running");
+    } else {
+        Command::new("openrgb")
+            .arg("--server")
+            .stdin(Stdio::null())
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .spawn()?;
 
-    tokio::time::sleep(Duration::from_millis(10000)).await;
-
-    let client = OpenRgbClient::connect().await?;
+        tokio::time::sleep(Duration::from_millis(10000)).await;
+    }
+    let client = OpenRgbClient::connect().await.unwrap_or_else(|_|
+        panic!("Could not connect to OpenRGB server"));
     let controllers = client.get_all_controllers().await?;
     controllers.init().await?;
 
     let mut device = Device::open(path)?;
-    let mut mic_enabled = true; // Assume mic is on
+    let mut mic_enabled = false; // Assume mic is on
 
     if let Some(keyboard) = controllers.into_iter().next() {
         set_color(&keyboard, mic_enabled).await?;
