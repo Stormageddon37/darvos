5ad369d3fba7af8d2977e9b04e0b412399521eba
Nitzan <bresler.nitzan@gmail.com>
2025-10-17 20:40:21 +0300

    change default values

--- src/consts.rs
+++ src/consts.rs
@@ -4,5 +4,4 @@ use std::time::Duration;
 pub const RETRY_DELAY: Duration = Duration::from_secs(2);
 pub const RED: Color = Color::new(255, 0, 0);
 pub const GREEN: Color = Color::new(0, 255, 0);
-pub const BLUE: Color = Color::new(0, 0, 255);
-pub const DEFAULT_COLOR: Color = BLUE;
+pub const DEFAULT_COLOR: Color = GREEN;
